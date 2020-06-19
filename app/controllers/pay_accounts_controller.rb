fb1553265436e18d41ac2b29ff5250ed4e58d8af
Hagan McPhail <hagan.lfd36@gmail.com>
2020-06-19 00:19:58 -0500

    Make the create button work and shows a form that allows a user to enter nickname, account number, and account type

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -1,12 +1,31 @@
 class PayAccountsController < ApplicationController
+  before_action :convert_radio, only: ["create"]
 
   # TODO: Add lookup to Account class
 
   def index
     @pay_accounts = PayAccount.all
+    @pay_account = PayAccount.new
   end
 
   def create
+    @pay_account = PayAccount.new(account_params)
+
+    if @pay_account.save
+      redirect_to root_path
+    else
+      ap @pay_account.errors.full_messages
+      redirect_to root_path, flash: { error: @pay_account.errors.full_messages }
+    end
   end
 
+  def account_params
+    params.require(:pay_account).permit(:nickname,
+                                        :account_type,
+                                        :account_number)
+  end
+
+  def convert_radio
+    params[:pay_account][:account_type] = params[:pay_account][:account_type].to_i
+  end
 end
--- app/models/pay_account.rb
+++ app/models/pay_account.rb
@@ -1,3 +1,6 @@
 class PayAccount < ActiveRecord::Base
   enum account_type: {bank_account:  1, debit_card: 2}
+  validates :nickname, :account_type, :account_number, presence: :true
+  validates :account_number, numericality: true, uniqueness: true
+  validates :nickname, uniqueness: true
 end
